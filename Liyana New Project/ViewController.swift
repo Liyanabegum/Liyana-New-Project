6027bc4cd321cd3c4b034171a9b524de5c5dc426
LIYANA <liyana@liyanas-mac.local>
2018-11-26 19:25:09 +0530

    i changed the subtraction task to AppStore

--- Liyana New Project/ViewController.swift
+++ Liyana New Project/ViewController.swift
@@ -8,24 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDelegate,UITableViewDataSource {
+   
+    
+    
+    @IBOutlet weak var appTableView: UITableView!
+    var appDataArray = NSMutableArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let a = 10
-        let b = 10
-        var sub = a - b
-        print(sub)
+        let  urlString = "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-free/all/10/explicit.json"
+       getAppDetails(AppStoreURL: urlString, onSuccess:{ (data) in
+            if let recievedData = data as? NSArray {
+                if recievedData.count > 0 {
+                self.appDataArray.addObjects(from: recievedData as! [Any])
+                DispatchQueue.main.async {
+                    self.appTableView.reloadData()
+                }
+            }
+            }
+        }) {(error) in
+            print(error.localizedDescription)
+        }
+    }
+  //artworkUrl100
+    func getAppDetails(AppStoreURL:String, onSuccess:@escaping ((Any)->Void), onFailure:@escaping ((Error)->Void)){
+        let url = URL(string: AppStoreURL)
+        let urlRequest = URLRequest(url: url!)
+        let dataTask = URLSession.shared.dataTask(with: urlRequest as URLRequest){data,response,error in
+            if (data != nil) {
+                do {
+                    let dataResponse = try JSONSerialization.jsonObject(with: data!, options: [])
+                     let receivedData = (((dataResponse as! NSDictionary).value(forKey: "feed") as! NSDictionary).value(forKey: "results"))
+                    onSuccess(receivedData  ?? NSArray() )
+                } catch let error {
+                    onFailure(error)
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.appDataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = appTableView.dequeueReusableCell(withIdentifier: "tableCell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "tableCell")
+        }
         
+        if let appDict = self.appDataArray.object(at: indexPath.row) as? NSDictionary {
+            cell?.textLabel?.text = appDict.value(forKey: "name") as? String
+            cell?.imageView?.image = appDict.value(forKey: "artworkUrl100") as? UIImage
+        } else {
+            cell?.textLabel?.text = "Not found"
+             cell?.imageView?.image = nil
+        }
         
-        // Do any additional setup after loading the view, typically from a nib.
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
